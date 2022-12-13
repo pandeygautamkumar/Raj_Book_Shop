1292fee33c2830dbe21795f395b07b7c1ab72c62
pandeygautamkumar <106687927+pandeygautamkumar@users.noreply.github.com>
2022-12-13 13:42:55 +0530

    Add files via upload

---
+++ frontend/build/precache-manifest.254e45fb2ba04e901907532826666f67.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "00ed1818eb7b6e11b8d9",
+    "url": "/static/css/main.2909cfb0.chunk.css"
+  },
+  {
+    "revision": "00ed1818eb7b6e11b8d9",
+    "url": "/static/js/main.00ed1818.chunk.js"
+  },
+  {
+    "revision": "962f1eb71e622add027f",
+    "url": "/static/css/1.052928b7.chunk.css"
+  },
+  {
+    "revision": "962f1eb71e622add027f",
+    "url": "/static/js/1.962f1eb7.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "782adc2b6062ab00461359da5b02b753",
+    "url": "/static/media/Profile.782adc2b.png"
+  },
+  {
+    "revision": "c229837f8adff0ab17f83612b56b0187",
+    "url": "/static/media/cover.c229837f.jfif"
+  },
+  {
+    "revision": "f159346a7b15253ba12bf56d64289ec3",
+    "url": "/index.html"
+  }
+];
